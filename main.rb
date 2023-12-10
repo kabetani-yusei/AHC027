490bbf71a5898d15458d7135a016eadc099b3b57
kabetani-yusei <31913@toyota.kosen-ac.jp>
2023-12-10 15:56:21 +0900

    rubyのやつは完成

--- main.rb
+++ main.rb
@@ -135,10 +135,13 @@ def cal_group_dist(pre, nex)
   root_first = []
   root_second = []
   root_flag = 0
+  set = []
   for k in 0..3
     flag = 4
     dist = Array.new(N){Array.new(N,-1)}
     i1, j1 = pre[look_list[k][0]], pre[look_list[k][1]]
+    next if set.include?([i1,j1])
+    set << [i1, j1]
     que = [[i1, j1]]
     dist[i1][j1] = 0
     while !que.empty?
@@ -204,6 +207,9 @@ def cal_group_dist(pre, nex)
       root_second.reverse!
     end
   end
+  if root_second == []
+    root_second = root_first.map(&:dup)
+  end
   return [root_first, root_second]
 end
 
@@ -211,7 +217,7 @@ $group_dist = Array.new($group_root.size){Array.new($group_root.size){Array.new(
 for i in 0...$group_root.size
   for j in (i+1)...$group_root.size
     $group_dist[i][j] = cal_group_dist(group_edge[i], group_edge[j])
-    $group_dist[j][i] = [$group_dist[i][j][0].reverse.map(&:dup), $group_dist[i][j][1].reverse.map(&:dup)]
+    $group_dist[j][i] = cal_group_dist(group_edge[j], group_edge[i])
   end
 end
 #0,0に戻るときの距離
@@ -252,7 +258,7 @@ now_dist = root_distance($group_root)
 time = Time.now
 size = $group_root.size
 if size > 1
-while Time.now - time < 2
+while Time.now - time < 0.2
   group_root_temp = $group_root.dup
   i = rand(1...size)
   j = rand(1...size)
@@ -269,60 +275,130 @@ while Time.now - time < 2
   end
 end
 end
-group_list.each{puts _1.join(" ")}
-p $group_root
-exit
+
 
 
 
 
 #つなげていく
-def group_inner_dfs(i, j, i_min, j_min, i_max, j_max)
+def group_inner_dfs(now_point, next_point, cover_range, flag)
+  i, j = now_point
   $dist[i][j] = 1
-  dij = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  imin, jmin, imax, jmax = cover_range
+  #最後の2マス間隔のときにflagを変える場合もある
+  if flag == 1 && [jmin, jmax].include?(j)
+    if (next_point[0] - i).abs == 1 && (next_point[1] - j).abs != 0
+      if $change_flag == 0
+        $change_flag = 1
+        flag = 2
+      end
+    end
+  elsif flag == 2 && [imin, imax].include?(i)
+    if (next_point[1] - j).abs == 1 && (next_point[0] - i).abs != 0
+      if $change_flag == 0
+        $change_flag = 1
+        flag = 1
+      end
+    end
+  end
+
+  if flag == 1#横優先
+    dij = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  else#縦優先
+    dij = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  end
   4.times do |d|
     di, dj = dij[d]
     i2, j2 = i + di, j + dj
-    if i_min <= i2 && i2 <= i_max && j_min <= j2 && j2 <= j_max && $dist[i2][j2] == 0
-      $inner_root  << [i2,j2]
-      group_inner_dfs(i2, j2, i_min, j_min, i_max, j_max)
+    if imin <= i2 && i2 <= imax && jmin <= j2 && j2 <= jmax && $dist[i2][j2] == 0
+      $group_inner_root_list << [i2, j2]
+      group_inner_dfs([i2, j2], next_point, cover_range, flag)
     end
   end
+end
+def group_inner_root(now_point,next_point,cover_range)
+  #例外処理
+  if now_point == next_point
+    return []
+  end
+  #now_pointからnext_pointへの移動
+  $group_inner_root_list = []
+  $dist = Array.new(N){Array.new(N, 0)}
+  $change_flag = 0
+  if now_point[0] == next_point[0]
+    #縦から始めたい
+    group_inner_dfs(now_point, next_point, cover_range, 2)
+  else
+    group_inner_dfs(now_point, next_point, cover_range, 1)
+  end
+
+  if $group_inner_root_list[-1] != next_point
+    $group_inner_root_list << next_point
+  end
+  return $group_inner_root_list
 end
 
 #0,0から0,0が含まれる長方形の頂点への移動
 ans_root = [[0,0]]
-$inner_root = []
-$dist = Array.new(N) { Array.new(N, 0) }
-i_min, j_min, i_max, j_max = group_edge[0]
-group_inner_dfs(0,0,i_min,j_min,i_max,j_max)
-$inner_root.each{|rrr| ans_root << rrr}
-end_point = ans_root[-1]
+now_point = [0,0]
+root_flag = 0
+if now_point == $group_dist[0][$group_root[1]][0][0]
+  next_point = $group_dist[0][$group_root[1]][1][0]
+  root_flag = 1
+else
+  next_point = $group_dist[0][$group_root[1]][0][0]
+end
+#頂点から頂点への移動
+inner_root = group_inner_root(now_point, next_point, group_edge[0])
+inner_root.each{|ir| ans_root << ir}
 
+#group->groupを繋げる
+outer_root = $group_dist[0][$group_root[1]][root_flag].map(&:dup)
+outer_root.shift
+outer_root.each{|io| ans_root << io}
+#now_pointの更新
+now_point = ans_root[-1]
 
 #頂点->頂点、塗りつぶしを繰り返す
-for group_num in 1...$group_root.size
-  #頂点->頂点
-  i, j = end_point
-  i_min, j_min, i_max, j_max = group_edge[$group_root[group_num]]
-  #ルートと最後の頂点を取得
-  #最初は含まれない最後は含まれる
-  group_to_grouop_root = group_to_group(i, j, i_min, j_min, i_max, j_max)
-  group_to_grouop_root.each{|rrr| ans_root << rrr}
-  end_point = ans_root[-1]
-  #塗りつぶし
-  #最初は含まれない最後は含まれる
-  $inner_root = []
-  $dist = Array.new(N) { Array.new(N, 0) }
-  group_inner_dfs(end_point[0],end_point[1],i_min,j_min,i_max,j_max)
-  $inner_root.each{|rrr| ans_root << rrr}
-  end_point = ans_root[-1]
+for group_num in 2...$group_root.size
+  root_flag = 0
+  now_group = $group_root[group_num - 1]
+  next_group = $group_root[group_num]
+  if now_point == $group_dist[now_group][next_group][0][0]
+    next_point = $group_dist[now_group][next_group][1][0]
+    root_flag = 1
+  else
+    next_point = $group_dist[now_group][next_group][0][0]
+  end
+  #頂点から頂点への移動
+  inner_root = group_inner_root(now_point, next_point, group_edge[now_group])
+  inner_root.each{|ir| ans_root << ir}
+  #group->groupを繋げる
+  outer_root = $group_dist[now_group][next_group][root_flag].map(&:dup)
+  outer_root.shift
+  outer_root.each{|io| ans_root << io}
+  #now_pointの更新
+  now_point = ans_root[-1]
 end
 
+
 #最後の頂点->0,0
-i, j = end_point
-group_to_grouop_root = group_to_group(i, j, 0, 0, 0, 0)
-group_to_grouop_root.each{|rrr| ans_root << rrr}
+root_flag = 0
+last_group = $group_root[-1]
+if now_point == $group_dist[last_group][last_group][0][0]
+  next_point = $group_dist[last_group][last_group][1][0]
+  root_flag = 1
+else
+  next_point = $group_dist[last_group][last_group][0][0]
+end
+#頂点から頂点への移動
+inner_root = group_inner_root(now_point, next_point, group_edge[last_group])
+inner_root.each{|ir| ans_root << ir}
+#group->[0,0]を繋げる
+outer_root = $group_dist[last_group][last_group][root_flag].map(&:dup)
+outer_root.shift
+outer_root.each{|io| ans_root << io}
+
 
 
 #答えを出力
